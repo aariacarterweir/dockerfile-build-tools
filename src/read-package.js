20a4427097aede19bd9361cb79a0dce517145754
Aaria Carter-Weir <aaria.carterweir@gmail.com>
2021-01-12 14:22:09 +1000

    v0.1.0

--- src/read-package.js
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env node
-
-const path = require("path");
-const [, , key] = process.argv;
-const packageJson = require(path.join(process.cwd(), "package.json"));
-console.log(packageJson[key]);
