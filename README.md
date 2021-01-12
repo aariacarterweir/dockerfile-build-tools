bbdd5661ae3dc50f7b19c6779ab86e42fef2b6c1
Aaria Carter-Weir <aaria.carterweir@gmail.com>
2021-01-12 14:53:20 +1000

    request new version if not set

--- README.md
+++ README.md
@@ -5,13 +5,11 @@ Easy way to build, version and push your local dockerfile to docker hub
 ## Installation
 - run `yarn add dockerfile-build-tools`
 - Either:
-    - Set up your package json with the docker-image-name & version fields matching the image name and
-        version of your docker hub target image
+    - Set up your package json with the docker-image-name of your docker target image
         ```json
           {
-            "docker-image-name": "aariacarterweir/docker-node-yarn",
-            "version": "0.1.4"
-          }       
+            "docker-image-name": "aariacarterweir/docker-node-yarn"
+          }
         ```
     - create a .dockerfile-build.env and set the following values
         ```dotenv
@@ -19,9 +17,8 @@ Easy way to build, version and push your local dockerfile to docker hub
           # optional setting of latest tag, defaults to "latest"
           LATEST_TAG=latest
         ```
-    - Or do a mix of both. Anything set in the env file will be given precedence over anything else
 - run `yarn dockerfile-build-tools` to build your Dockerfile and tag it as latest
 - run `yarn dockerfile-build-tools -v 0.2.1` to manually set a specific version
-- run `yarn dockerfile-build-tools -p` to push your versioned docker image to docker-hub
+- run `yarn dockerfile-build-tools -p` to push your versioned docker image to docker-hub (it will ask you for a new version)
 - run `yarn dockerfile-build-tools -v 0.2.2 -p` to combine a manual version override and also
     push your image to docker-hub
--- build.js
+++ build.js
@@ -1,43 +1,59 @@
 #!/usr/bin/env node
 
-const { execSync } = require('child_process');
-const path = require('path');
-const fs = require('fs');
-const dotenv = require('dotenv');
-const argv = require('minimist')(process.argv.slice(2));
-const packageJson = require(path.resolve(process.cwd(), 'package.json'));
+module.exports = async (args) => {
+    const { execSync } = require('child_process');
+    const path = require('path');
+    const fs = require('fs');
+    const dotenv = require('dotenv');
+    const argv = require('minimist')(args);
 
-// alias execSync for eases of the uses
-const ex = (cmd, options = {}) => execSync(cmd, { stdio: 'inherit', ...options });
+    const packageJsonPath = path.resolve(process.cwd(), 'package.json');
+    const packageJson = require(packageJsonPath);
 
-// load in env file if it exists
-if (fs.existsSync(path.resolve(process.cwd(), '.dockerfile-build.env'))) {
-    dotenv.config({ path: path.resolve(process.cwd(), '.dockerfile-build.env') });
-}
+    // alias execSync for eases of the uses
+    const ex = (cmd, options = {}) => execSync(cmd, {stdio: 'inherit', ...options});
 
-// extract args from cli / package.json
-const {
-    v: VERSION = packageJson.version,
-    p: PUSH = false
-} = argv;
+    // sort out a default for v arg (version argument)
+    if (! argv.v) {
+        const { prompt } = require('enquirer');
+        const writePkg = require('write-pkg');
 
-// extract image details from env / package.json
-const {
-    IMAGE_NAME = packageJson['docker-image-name'],
-    LATEST_TAG = packageJson['docker-latest-tag'] || 'latest',
-} = process.env;
+        const response = await prompt({
+            type: 'input',
+            name: 'version',
+            message: `Current version: ${packageJson.version}, new version?`
+        });
 
-// build
-ex(`docker build -t ${IMAGE_NAME}:${LATEST_TAG} .`);
+        packageJson.version = response.version;
+        writePkg(packageJsonPath, packageJson);
+    }
 
-if (VERSION) {
+    // extract args from cli / package.json
+    const {
+        v: VERSION = packageJson.version,
+        p: PUSH = false
+    } = argv;
+
+    // load in env file if it exists
+    if (fs.existsSync(path.resolve(process.cwd(), '.dockerfile-build.env'))) {
+        dotenv.config({path: path.resolve(process.cwd(), '.dockerfile-build.env')});
+    }
+
+    // extract image details from env / package.json
+    const {
+        IMAGE_NAME = packageJson['docker-image-name'],
+        LATEST_TAG = packageJson['docker-latest-tag'] || 'latest',
+    } = process.env;
+
+    // build & tag
+    ex(`docker build -t ${IMAGE_NAME}:${LATEST_TAG} .`);
     ex(`docker tag ${IMAGE_NAME}:${LATEST_TAG} ${IMAGE_NAME}:${VERSION}`);
-}
 
-if (PUSH) {
-    ex(`docker push ${IMAGE_NAME}:${LATEST_TAG}`);
-
-    if (VERSION) {
+    // Push
+    if (PUSH) {
+        ex(`docker push ${IMAGE_NAME}:${LATEST_TAG}`);
         ex(`docker push ${IMAGE_NAME}:${VERSION}`)
     }
 }
+
+module.exports(process.argv.slice(2));
