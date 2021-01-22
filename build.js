793e3ef940e83c1dcea44b75d7d2d219c5737cba
Aaria Carter-Weir <aaria.carterweir@gmail.com>
2021-01-22 21:06:20 +1000

    v0.1.14

--- build.js
+++ build.js
@@ -1,43 +1,55 @@
 #!/usr/bin/env node
 
-module.exports = async (args) => {
+module.exports = async (...args) => {
     const { execSync } = require('child_process');
     const path = require('path');
     const fs = require('fs');
     const dotenv = require('dotenv');
-    const argv = require('minimist')(args);
-
-    const packageJsonPath = path.resolve(process.cwd(), 'package.json');
-    const packageJson = require(packageJsonPath);
-
-    // alias execSync for eases of the uses
     const ex = (cmd, options = {}) => execSync(cmd, {stdio: 'inherit', ...options});
 
-    // sort out a default for v arg (version argument)
-    if (! argv.v) {
-        const { prompt } = require('enquirer');
-        const writePkg = require('write-pkg');
+    // parse args
+    const argv = require('minimist')(args, {
+        boolean: [
+            'non-interactive',
+        ],
+    });
 
-        const response = await prompt({
-            type: 'input',
-            name: 'version',
-            message: `Current version: ${packageJson.version}, new version?`,
-            initial: packageJson.version,
-        });
+    const workingDir = argv['working-dir'] || process.cwd();
+    const packageJsonPath = path.resolve(workingDir, 'package.json');
+    const packageJson = require(packageJsonPath);
 
-        packageJson.version = response.version;
-        writePkg(packageJsonPath, packageJson);
+    interactive: {
+        if (argv['non-interactive']) {
+            break interactive;
+        }
+
+        // sort out a default for v arg (version argument)
+        if (! argv.v) {
+            const { prompt } = require('enquirer');
+            const writePkg = require('write-pkg');
+
+            const response = await prompt({
+                type: 'input',
+                name: 'version',
+                message: `Current version: ${packageJson.version}, new version?`,
+                initial: packageJson.version,
+            });
+
+            argv.v = response.version;
+            packageJson.version = response.version;
+            writePkg(packageJsonPath, packageJson);
+        }
     }
 
     // extract args from cli / package.json
     const {
-        v: VERSION = packageJson.version,
+        v: VERSION = false,
         p: PUSH = false
     } = argv;
 
     // load in env file if it exists
-    if (fs.existsSync(path.resolve(process.cwd(), '.dockerfile-build.env'))) {
-        dotenv.config({path: path.resolve(process.cwd(), '.dockerfile-build.env')});
+    if (fs.existsSync(path.resolve(workingDir, '.dockerfile-build.env'))) {
+        dotenv.config({path: path.resolve(workingDir, '.dockerfile-build.env')});
     }
 
     // extract image details from env / package.json
@@ -48,13 +60,23 @@ module.exports = async (args) => {
 
     // build & tag
     ex(`docker build -t ${IMAGE_NAME}:${LATEST_TAG} .`);
-    ex(`docker tag ${IMAGE_NAME}:${LATEST_TAG} ${IMAGE_NAME}:${VERSION}`);
+
+    if (VERSION) {
+        ex(`docker tag ${IMAGE_NAME}:${LATEST_TAG} ${IMAGE_NAME}:${VERSION}`);
+    }
 
     // Push
     if (PUSH) {
         ex(`docker push ${IMAGE_NAME}:${LATEST_TAG}`);
         ex(`docker push ${IMAGE_NAME}:${VERSION}`)
     }
+
+    return {
+        IMAGE_NAME,
+        LATEST_TAG,
+        VERSION,
+        PUSH,
+    };
 }
 
-module.exports(process.argv.slice(2));
+module.exports(...process.argv.slice(2));
